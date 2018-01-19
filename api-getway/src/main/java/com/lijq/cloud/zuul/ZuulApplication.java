5439a3cb73a3f306dba65482e54e6c84fe47d96d
Lijq <ly_0199@163.com>
2018-01-19 16:46:55 +0800

    Add Zuul

--- api-getway/src/main/java/com/lijq/cloud/zuul/ZuulApplication.java
+++ api-getway/src/main/java/com/lijq/cloud/zuul/ZuulApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringCloudApplication
 @EnableZuulProxy
+//@EnableDiscoveryClient
 public class ZuulApplication {
 
     public static void main(String[] args) {
